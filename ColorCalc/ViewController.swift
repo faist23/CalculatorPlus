fd1c12317ffceab0b95cda9f95e5bc46bb7438ea
Craig Faist <cdf@craigs-mbp>
2017-02-12 18:56:03 -0500

    Initial Commit

---
+++ ColorCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorCalc
+//
+//  Created by Craig Faist on 2/12/17.
+//  Copyright © 2017 Spectrum Image, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
