2a5c93066b4dd5d588bc1fee23eac03c8f300baa
Craig Faist <cdf@Craigs-MacBook-Pro.local>
2017-03-07 13:04:09 -0500

    initial commit

--- ColorCalc/ViewController.swift
+++ ColorCalc/ViewController.swift
@@ -7,19 +7,296 @@
 //
 
 import UIKit
+import GoogleMobileAds // google mobile ads
 
-class ViewController: UIViewController {
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
 
+
+class ViewController: UIViewController, UIInputViewAudioFeedback, GADBannerViewDelegate { // google mobile ads
+    @IBOutlet weak var label: UILabel!
+    
+    let bannerView: GADBannerView = GADBannerView.init(adSize: kGADAdSizeSmartBannerPortrait)
+    
+    var enableInputClicksWhenVisible: Bool {
+        return true
+    }
+    
+    var labelString:String = "0"
+    var operator1:String = ""
+    var equation:String = ""
+    var currentMode:modes = .not_set
+    var saveNum:Double = 0
+    var saveNum2:Double = 0
+    var lastButtonWasMode:Bool = false
+    var decimalUsed = false
+    var memoryNum:Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupAdBanner()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func didPressSettings(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var memUsedLabel: UILabel!
+    @IBOutlet weak var labelDarkGray: UILabel!
+    @IBOutlet weak var labelBlack: UILabel!
+    
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        operator1 = "+"
+        changeMode(newMode: .addition)
+    }
+    @IBAction func didPressMinus(_ sender: Any) {
+        operator1 = "-"
+        changeMode(newMode: .subtraction)
+    }
+    @IBAction func didPressMultiply(_ sender: Any) {
+        operator1 = "*"
+        changeMode(newMode: .multiplication)
+    }
+    @IBAction func didPressDivide(_ sender: Any) {
+        operator1 = "/"
+        changeMode(newMode: .division)
+    }
+    
+    @IBAction func didPressMClear(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        memoryNum = 0
+        memUsedLabel.textColor = labelDarkGray.textColor
+    }
+    @IBAction func didPressMPlus(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        memoryNum += Double(labelString)!
+        memUsedLabel.textColor = labelBlack.textColor
+        lastButtonWasMode = true
+    }
+    @IBAction func didPressMMinus(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        memoryNum -= Double(labelString)!
+        memUsedLabel.textColor = labelBlack.textColor
+        lastButtonWasMode = true
+    }
+    @IBAction func didPressMRecall(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        //        currentMode = .not_set
+        labelString = "\(memoryNum)"
+        updateText()
+        lastButtonWasMode = true
+        decimalUsed = false
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+        if currentMode == .not_set  {  // || lastButtonWasMode
+            return
+        }
+        
+        equation = equation + labelString // testing equation for correct math function
+        
+        currentMode = .not_set
+  
+        print(equation)
+        let expression = NSExpression(format: equation)
+        saveNum2 = expression.expressionValue(with: nil, context: nil) as! Double
+        
+        print(saveNum2,saveNum,equation)
+        labelString = "\(saveNum2)"
+        equation = ""
+        
+        updateText()
+        lastButtonWasMode = true
+        decimalUsed = false
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        if (sender as AnyObject).titleLabel??.text == "AC" {
+            (sender as AnyObject).setTitle("C", for: .normal)
+            currentMode = .not_set
+            saveNum = 0
+            saveNum2 = 0
+            lastButtonWasMode = false
+        } else if currentMode != .not_set {
+            (sender as AnyObject).setTitle("AC", for: .normal)
+        }
+        labelString = "0"
+        label.text = "0"
+    }
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        if sender.titleLabel?.text == "." && decimalUsed {
+            return
+        } else {
+            
+            if sender.titleLabel?.text == "." {
+                decimalUsed = true
+            }
+            
+            if lastButtonWasMode {
+                lastButtonWasMode = false
+                labelString = "0"
+            }
+            if labelString.characters.count > 10 {
+                return
+            }
+            labelString = labelString.appending(stringValue!)
+            updateText()
+        }
+    }
+    
+    @IBAction func didPressPlusMinus(_ sender: Any) {
+        UIDevice.current.playInputClick()
+        if labelString[labelString.startIndex] == "-" {
+            labelString.remove(at: labelString.startIndex)
+        } else {
+            labelString = "-" + labelString
+        }
+        updateText()
+    }
+    
+    @IBAction func didPressPercent(_ sender: Any) {
+        if currentMode == .not_set {
+            labelString = "\(Double(labelString)! / 100)"
+            updateText()
+            return
+        }
+        if currentMode == .addition {
+            labelString = "\(saveNum * Double(labelString)! / 100)"
+        } else if currentMode == .subtraction {
+            labelString = "\(saveNum * Double(labelString)! / 100)"
+        } else if currentMode == .multiplication {
+            labelString = "\(Double(labelString)! / 100)"
+        } else if currentMode == .division {
+            labelString = "\(Double(labelString)! / 100)"
+        }
+        updateText()
+    }
+    @IBAction func didPressDelete(_ sender: Any) {
+        if lastButtonWasMode {
+            return
+        }
+        UIDevice.current.playInputClick()
+        if labelString == "0" {
+            labelString = "0"
+            label.text = "0"
+        } else {
+            if labelString[labelString.index(before: labelString.endIndex)] == "." {
+                decimalUsed = false
+            }
+            labelString.remove(at: labelString.index(before: labelString.endIndex))
+            updateText()
+        }
+        
+    }
+    
+    func updateText() {
+        UIDevice.current.playInputClick()
+        guard let labelNbr:Double = Double(labelString) else {
+            return
+        }
+        if (currentMode == .not_set) {
+            saveNum = labelNbr
+        }
+        let formatter:NumberFormatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 9
+        let num:NSNumber = NSNumber(value: labelNbr)
+        label.text = formatter.string(from: num)
+        if labelString[labelString.index(before: labelString.endIndex)] == "." {
+            label.text = label.text! + "."
+        }
+    }
+    
+    func changeMode(newMode:modes) {
+            equation = equation + "\(Double(labelString)!)"
 
+        if currentMode != .not_set && (newMode == .addition || newMode == .subtraction) {
+            let exp: NSExpression = NSExpression(format: equation)
+            saveNum = exp.expressionValue(with: nil, context: nil) as! Double
+            equation = "\(saveNum)"
+            labelString = "\(saveNum)"
+            updateText()
+        }
+        equation = equation + operator1
+print(equation)
+        currentMode = newMode
+        lastButtonWasMode = true
+        decimalUsed = false
 
+  print(currentMode,newMode,saveNum,saveNum2)
+    }
+    
+    
+    // google ads
+    func setupAdBanner() {
+        // set the banner orientation when the view loads
+        let currentDevice: UIDevice = UIDevice.current
+        let orientation: UIDeviceOrientation = currentDevice.orientation
+        
+        if orientation.isLandscape {
+            setBannerOrientation(portrait: false)
+        }
+
+        // Load an ad in the usual way
+        print("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
+        bannerView.adUnitID = "ca-app-pub-2036170160247702/7529154872"
+        bannerView.rootViewController = self
+        let request = GADRequest()
+        
+        // google test ads begin
+        request.testDevices = [ kGADSimulatorID, "5be20290b246c80a40a8a5748a64c1f8" ]
+        // google test ads end
+        
+        bannerView.load(request)
+        self.bannerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // create contraints to centre the add and pin to the bottom of the view
+        let xConstraint = NSLayoutConstraint(item: bannerView, attribute: .centerX,
+                                             relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0)
+        
+        let pinBottom = NSLayoutConstraint(item: bannerView, attribute: .bottom,
+                                           relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        // must add to sub view prior to adding constraints
+        self.view.addSubview(bannerView)
+        
+        self.view.addConstraint(xConstraint)
+        self.view.addConstraint(pinBottom)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        // detect orientation change and adjust banner accordingly
+        if UIDevice.current.orientation.isLandscape {
+            setBannerOrientation(portrait: false)
+        } else {
+            setBannerOrientation(portrait: true)
+        }
+    }
+    
+    func setBannerOrientation( portrait: Bool ){
+        if portrait {
+            self.bannerView.adSize = kGADAdSizeSmartBannerPortrait;
+        } else {
+            // Landscape
+            self.bannerView.adSize = kGADAdSizeSmartBannerLandscape;
+        }
+    }
+    // google ads
 }
 
